fd248ffad06c0759959e9ee53039c031f2234678
Cecilie Haugstvedt <anneceh@users.noreply.github.com>
2020-03-02 22:52:06 +0100

    Initial commit, adds browser test

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # axe-webdriverjs
 Example project for writing browser tests with axe-webdriverjs
+
+Prerequisites
+Install node.js
+Install chromedriver
+npm install chromedriver
---
+++ spec/test.js
@@ -0,0 +1,62 @@
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var chromedriver = require('chromedriver');
+
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+var driver = new webdriver.Builder()
+                 .withCapabilities(webdriver.Capabilities.chrome())
+                 .build();
+
+var AxeBuilder = require('axe-webdriverjs'),
+    Key = webdriver.Key;
+
+var util = require('util');
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+describe('Axe WebDriver JS demo', function() {
+
+    beforeEach(function(done) {
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build();
+
+            driver.get('https://clothes4all.netlify.com/').then(function () {
+                  driver.executeAsyncScript(function(callback) {
+                      var script = document.createElement('script');
+                      script.innerHTML = 'document.documentElement.classList.add("deque-axe-is-ready");';
+                      document.documentElement.appendChild(script);
+                      callback();
+                  })
+                  .then(function () {
+                      return driver.wait(webdriver.until.elementsLocated(webdriver.By.css('.deque-axe-is-ready')));
+                  })
+                  .then(function () {
+                      done();
+                  });
+              });
+    });
+
+    // Close website after each test is run (so it is opened fresh each time)
+    afterEach(function(done) {
+        driver.quit().then(function () {
+            done();
+        });
+    });
+
+    it('should fetch webpage and analyze it', function (done) {
+      driver.findElement(webdriver.By.tagName('body'))
+          .then(function () {
+              AxeBuilder(driver)
+                  .analyze(function(results) {
+                      console.log('Accessibility Violations: ', results.violations.length);
+                      if (results.violations.length > 0) {
+                          console.log(util.inspect(results.violations, true, null));
+                      }
+                      expect(results.violations.length).toBe(0);
+                      done();
+                  })
+          });
+    });
+});
