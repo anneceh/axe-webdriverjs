16eaced30c8f0e7fba594d7105addede37c4118a
Cecilie Haugstvedt <anneceh@users.noreply.github.com>
2020-06-04 23:01:21 +0200

    Updates dependencies

--- spec/test.js
+++ spec/test.js
@@ -45,10 +45,7 @@ describe('Axe WebDriver JS demo', function() {
           .then(function () {
               AxeBuilder(driver)
                   .analyze(function(err, results) {
-                      // if (err) {
-                      //   return done(err);
-                      // }
-                      //console.log('Accessibility Violations: ', results.violations.length);
+                      console.log('Accessibility Violations: ', results.violations.length);
                       if (results.violations.length > 0) {
                           console.log(util.inspect(results.violations, true, null));
                       }
